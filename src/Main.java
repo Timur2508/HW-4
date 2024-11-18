f8d19c63d55cd22f019a69dec106a6c02d8cc7a0
Timur2508 <dispetcher89@list.ru>
2024-11-18 13:35:51 +0500

    DZ

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание");
+        System.out.println("Задача №1");
+
+        int age = 18;
+        if (age >= 17) {
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний ");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", возраст совершеннолетия еще не наступил, нужно немного подождать ");
+        }
+
+    }
+}
